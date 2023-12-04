95a72ecaa1f18de0b4cabb282d467d7d2007e64c
Daniel Campillo Ricardi <dcampillo05@gmail.com>
2023-12-03 23:52:43 -0300

    d

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'todos/index.html')
+
+def sobre(request):
+    return render(request, 'todos/sobre.html')
